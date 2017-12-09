7795b49cf674b15711c479d7b6bd2057892f5374
Hatem Ali <haris.syafa5@gmail.com>
2017-12-09 10:10:55 +0700

    Signed-off-by: Hatem Ali <haris.syafa5@gmail.com>

--- f_daftar.php
+++ f_daftar.php
@@ -18,7 +18,7 @@ $d_fj = mysqli_fetch_array($q_fj);
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Pendaftaran Wisuda Online Baru</title>
+<title>Pendaftaran Wisuda Online</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link href="style.css" rel="stylesheet" type="text/css" />
 
