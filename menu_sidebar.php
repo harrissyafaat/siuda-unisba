77f2589b7582d839293aa2da707f0392bc1293b6
Hatem Ali <haris.syafa5@gmail.com>
2017-12-09 10:16:28 +0700

    Merubah Nama Menu

--- menu_sidebar.php
+++ menu_sidebar.php
@@ -1,7 +1,7 @@
 
 <?php
 $link = "http://$_SERVER[HTTP_HOST]/wisuda";
-echo "<li class='active'><a style='font-size: 12px;' href='$link'>Beranda</a></li>";
+echo "<li class='active'><a style='font-size: 12px;' href='$link'>Berandaku</a></li>";
 echo "<li><a style='font-size: 12px;' href='jadwal.php'>Jadwal Pendaftaran Wisuda</a></li>";
 echo "<li><a style='font-size: 12px;' href='alur.php'>Alur Pendaftaran Wisuda</a></li>";
 echo "<li><a style='font-size: 12px;' href='daftar.php'>Form Pendaftaran Wisuda</a></li>";
